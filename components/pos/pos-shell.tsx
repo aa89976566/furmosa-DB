3314a496c9822856f8ab7fd393d04a92ef474200
aa89976566 <aa89976566@gmail.com>
2026-08-23 16:39:34 +0200

    Update pos-shell.tsx

--- components/pos/pos-shell.tsx
+++ components/pos/pos-shell.tsx
@@ -2,9 +2,11 @@ import { PosBottomNav } from '@/components/pos/bottom-nav';
 
 export function PosShell({ children }: { children: React.ReactNode }) {
   return (
-    <div className="mx-auto min-h-screen w-full max-w-5xl bg-[#f6f6f7] pb-24 text-[#191919]">
-      {children}
+    <div className="min-h-screen bg-[#f6f6f7] text-[#191919]">
       <PosBottomNav />
+      <main className="min-h-screen pb-24 md:pl-[232px] md:pb-0">
+        {children}
+      </main>
     </div>
   );
 }
