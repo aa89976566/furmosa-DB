43fcebbfb185922ef5cbb38ce34e0569c37d35b7
ming <ming@mingdeMacBook-Air.local>
2026-09-16 22:01:33 +0200

    fix: remove merchant shipping form syntax error

--- components/merchants/merchant-shipping-form.tsx
+++ components/merchants/merchant-shipping-form.tsx
@@ -178,7 +178,6 @@ export function MerchantShippingForm({
             className="block w-full rounded-lg border border-input bg-background px-3 py-2 text-sm focus:outline-none focus:ring-2 focus:ring-ring"
           />
         </MerchantField>
-        </MerchantField>
       </div>
 
       <MerchantFormActions className="border-t-0 pt-0">
